Fixed batch calculation size problem(was hardcoded 10 in some places). returned functionality of internal augmentation server. Since it very fast now it is obvious choice over zmq server Implemented parallel training, it works, but for my training usage of cars is really not that good. Also it renames losses to concatenate_XXX loss, should make patch to keras Started to do CoCo eval instead of useless accuracy I've removed, only pushed ground truth for metric yet. weights no stored in separate files, last file taken for training.

model.py
@@ -5,7 +5,7 @@ from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.merge import Multiply
 from keras.regularizers import l2
-from keras.initializers import random_normal,constant
+from keras.initializers import random_normal, constant
 
 stages = 6
 np_branch1 = 38
@@ -114,7 +114,7 @@ def apply_mask(x, mask1, mask2, num_p, stage, branch):
     return w
 
 
-def get_training_model(weight_decay):
+def get_training_model(weight_decay, gpus=None):
 
     img_input_shape = (None, None, 3)
     vec_input_shape = (None, None, 38)
@@ -165,7 +165,12 @@ def get_training_model(weight_decay):
         if (sn < stages):
             x = Concatenate()([stageT_branch1_out, stageT_branch2_out, stage0_out])
 
-    model = Model(inputs=inputs, outputs=outputs)
+    if gpus is None:
+        model = Model(inputs=inputs, outputs=outputs)
+    else:
+        import tensorflow as tf
+        with tf.device('/cpu:0'): #this model will not be actually used, it's template
+            model = Model(inputs=inputs, outputs=outputs)
 
     return model
 

py_rmpe_server/py_rmpe_data_iterator.py
@@ -1,13 +1,14 @@
 
 import h5py
-from py_rmpe_config import RmpeGlobalConfig, RmpeCocoConfig
 import random
-from py_rmpe_transformer import Transformer, AugmentSelection
-from py_rmpe_heatmapper import Heatmapper
 import json
 import numpy as np
 
-class DataIterator:
+from py_rmpe_server.py_rmpe_config import RmpeGlobalConfig, RmpeCocoConfig
+from py_rmpe_server.py_rmpe_transformer import Transformer, AugmentSelection
+from py_rmpe_server.py_rmpe_heatmapper import Heatmapper
+
+class RawDataIterator:
 
     def __init__(self, h5file, shuffle = True, augment = True):
 
@@ -18,7 +19,7 @@ class DataIterator:
         self.augment = augment
         self.shuffle = shuffle
 
-    def iterate(self):
+    def gen(self, dbg=False):
 
         keys = list(self.datum.keys())
 
@@ -30,9 +31,6 @@ class DataIterator:
             image, mask, meta = self.read_data(key)
             debug = {}
 
-            print("[in] IMAGE:", image.shape, image.dtype, meta['img_path'])
-            print("[in] MASK:", mask.shape, mask.dtype, meta['mask_miss_path'])
-
             debug['img_path']=meta['img_path']
             debug['mask_miss_path'] = meta['mask_miss_path']
             debug['mask_all_path'] = meta['mask_all_path']
@@ -40,7 +38,7 @@ class DataIterator:
             image, mask, meta, labels = self.transform_data(image, mask, meta)
             image = np.transpose(image, (2, 0, 1))
 
-            yield image, mask, labels, debug
+            yield image, mask, labels, meta['joints']
 
     def num_keys(self):
         return len(list(self.datum.keys()))

py_rmpe_server/py_rmpe_heatmapper.py
@@ -3,7 +3,7 @@
 import numpy as np
 from math import sqrt, isnan
 
-from py_rmpe_config import RmpeGlobalConfig, TransformationParams
+from py_rmpe_server.py_rmpe_config import RmpeGlobalConfig, TransformationParams
 
 class Heatmapper:
 
@@ -12,7 +12,7 @@ class Heatmapper:
         self.double_sigma2 = 2 * sigma * sigma
         self.thre = thre
 
-        # casched common parameters which same for all iterations and all pictures
+        # cached common parameters which same for all iterations and all pictures
 
         stride = RmpeGlobalConfig.stride
         width = RmpeGlobalConfig.width//stride
@@ -47,24 +47,10 @@ class Heatmapper:
             exp_x = np.exp(-(self.grid_x-joints[i,0])**2/self.double_sigma2)
             exp_y = np.exp(-(self.grid_y-joints[i,1])**2/self.double_sigma2)
 
-            #if layer==0:
-            #    print("%d, joint: %f, EXP_X: %s" % (layer, joints[i,0], exp_x))
-            #    print("%d, joint: %f, EXP_Y: %s" % (layer, joints[i,1], exp_y))
-
             exp = np.outer(exp_y, exp_x)
 
-            #if layer==0:
-            #    print("%d, EXP: %s" % (layer, exp))
-
-            # current way is sum of exps/trim at 1.0. May be best way is just max of exps
-            # heatmaps[RmpeGlobalConfig.heat_start + layer, :, :] += exp
-
-            # TODO: this is kinda strange but I've read original article after code and found they really use maximum. But in C++ code sum+trim.
-            # entry[g_y * grid_x + g_x] += exp(-exponent);
-            # if (entry[g_y * grid_x + g_x] > 1)
-            #    entry[g_y * grid_x + g_x] = 1;
-
-            # I will do maximum like in article
+            # note this is correct way of combination - min(sum(...),1.0) as was in C++ code is incorrect
+            # https://github.com/ZheC/Realtime_Multi-Person_Pose_Estimation/issues/118
             heatmaps[RmpeGlobalConfig.heat_start + layer, :, :] = np.maximum(heatmaps[RmpeGlobalConfig.heat_start + layer, :, :], exp)
 
     def put_joints(self, heatmaps, joints):
@@ -98,6 +84,12 @@ class Heatmapper:
 
             min_sx, max_sx = (x1, x2) if x1 < x2 else (x2, x1)
             min_sy, max_sy = (y1, y2) if y1 < y2 else (y2, y1)
+
+            # TODO: check PAF off screen.
+            # looks like slices working properly
+            # > foo=np.array([1,2,3,4,5,6,7,8,9,10])
+            # > foo[slice(-10,5)]
+            # array([1, 2, 3, 4, 5])
 
             min_sx = int((min_sx - self.thre) / RmpeGlobalConfig.stride)
             min_sy = int((min_sy - self.thre) / RmpeGlobalConfig.stride)

py_rmpe_server/rmpe_server.py
@@ -2,27 +2,31 @@
 import sys
 import numpy as np
 import zmq
-
 from multiprocessing import Process
-from py_rmpe_data_iterator import DataIterator
-
 from time import time
+
+sys.path.append("..")
+
+from py_rmpe_data_iterator import RawDataIterator
+
 
 class Server:
 
     # these methods all called in parent process
 
-    def __init__(self, h5file, port, name):
+    def __init__(self, h5file, port, name, shuffle, augment):
 
         self.name = name
         self.port = port
         self.h5file = h5file
 
+        self.shuffle = shuffle
+        self.augment = augment
+
         self.process = Process(target=Server.loop, args=(self,))
         self.process.daemon = True
         self.process.start()
 
-        self.heatmapper = None
 
 
     def join(self):
@@ -44,7 +48,7 @@ class Server:
         print("%s: Child process init... " % self.name)
         self.init()
 
-        iterator = DataIterator(self.h5file, shuffle=False, augment=True)
+        iterator = RawDataIterator(self.h5file, shuffle=self.shuffle, augment=self.augment)
 
         print("%s: Loop started... " % self.name)
 
@@ -54,45 +58,39 @@ class Server:
 
         while True:
 
-
             keys = iterator.num_keys()
             print("%s: generation %s, %d images " % (self.name, generation, keys))
 
             start = time()
-            for (image, mask, labels, debug) in iterator.iterate():
+            for (image, mask, labels, keypoints) in iterator.gen():
 
-                headers = self.produce_headers(image, mask, labels)
-                print("[out] HEADERS:",headers)
+                augment_time = time()-start
+
+                headers = self.produce_headers(image, mask, labels, keypoints)
                 self.socket.send_json(headers)
-                print("[out] IMAGE:", image.shape, image.dtype, np.max(image))
                 self.socket.send(np.ascontiguousarray(image))
-                print("[out] MASK:", mask.shape, mask.dtype, np.max(image))
                 self.socket.send(np.ascontiguousarray(mask))
-                print("[out] LABELS:", labels.shape, labels.dtype, np.max(labels) )
                 self.socket.send(np.ascontiguousarray(labels))
+                self.socket.send(np.ascontiguousarray(keypoints))
 
                 num += 1
-                print("[%d/%d] %0.2f images per second (last image %0.2f ms)" % (num, keys, num/(time() - cycle_start), (time() - start)*1000 ) )
+                print("%s [%d/%d] aug %0.2f ms (%0.2f im/s), send %0.2f ms" % (self.name, num, keys, augment_time*1000, 1./augment_time,  time() - start - augment_time) )
                 start = time()
 
-                print()
-                print()
-
-
-    def produce_headers(self, img, mask, labels):
+    def produce_headers(self, img, mask, labels, keypoints):
 
         header_data = {"descr": img.dtype.str, "shape": img.shape, "fortran_order": False}
         header_mask = {"descr": mask.dtype.str, "shape": mask.shape,   "fortran_order": False}
         header_label = {"descr": labels.dtype.str,  "shape": labels.shape, "fortran_order": False}
-        headers = [header_data, header_mask, header_label]
+        header_keypoints = {"descr": keypoints.dtype.str,  "shape": keypoints.shape, "fortran_order": False}
 
         return headers
 
 
 def main():
 
-    train = Server("../dataset/train_dataset.h5", 5555, "Train")
-    val = Server("../dataset/val_dataset.h5", 5556, "Val")
+    train = Server("../dataset/train_dataset.h5", 5555, "Train", shuffle=False, augment=True)
+    val = Server("../dataset/val_dataset.h5", 5556, "Val", shuffle=False, augment=False)
 
     processes = [val, train] #,
 